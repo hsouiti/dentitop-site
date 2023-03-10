312fede68eb4b2c9e23e3fc0eb3b5c4540c4eb92
Hassan <h.souiti@gmail.com>
2023-03-11 00:25:33 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { Inter, Roboto, Ubuntu  } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap'
+})
+
+
+const ubuntu = Ubuntu({
+    weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap'
+})
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={ubuntu.className}>
+      <body>
+        <div className="main" style={{display: 'flex', justifyContent: "center", }}>
+
+        {children}
+        </div>
+        </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import Logiciel from '../public/logiciel-gestion-cabinet-dentaire.png';
+import Link from 'next/link';
+
+
+
+export default function Page() {
+  return (
+    <>
+        <div className="container" style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '20px 60px',
+            border: '1px solid #a8a8a8',
+            flexDirection: 'column'
+        }}>
+               <h1 style={{textTransform: "capitalize", color: '#A31C88'}}>Site Bientôt En Ligne</h1>
+
+                  <div >
+            <div style={{ width: '100%', textAlign: 'center', margin: '20px 0'}}>
+                  <Image
+                  src={Logiciel}
+                  alt="Dentitop"
+                  width={300} 
+                  // height={500} automatically provided
+                  // blurDataURL="data:..." automatically provided
+                  // placeholder="blur" // Optional blur-up while loading
+                  />
+      </div>
+                  <h2 style={{margin: 0,  }}>Dentitop</h2>
+                  <h3 style={{fontWeight: '400', marginTop: '10px', marginBottom: 0 }}>Un logiciel dentaire moderne et complet </h3>
+                  <h4 style={{fontWeight: '500', marginTop: '10px' }}>Contact: 06 01 37 11 24 - dentitop1@gmail.com</h4>
+                  <p style={{color: '#A31C88', fontSize: '1rem', fontWeight: 500}}>Facile d'utilisation, Ergonomique, evolutif en permanence</p>
+                <Link
+                  style={{
+                    marginTop: '25px',
+                     display: 'block', 
+                    background: '#A31C88', 
+                    color: 'white', 
+                    padding: '12px 15px',
+                    borderRadius: '8px',
+                    textAlign: 'center'
+                }}
+                  href='/brochure-dentitop.pdf'
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
+                    Télécharger Brochure Dentitop
+                </Link>
+                  </div>
+
+      
+       
+    
+    </div> 
+  
+       
+  
+      </>
+  );
+}
+
